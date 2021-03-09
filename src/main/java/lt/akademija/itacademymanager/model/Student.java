faf347fb80e1c23919b557813ed624ed23cf3d43
GVabal <gvabal@gmail.com>
2021-03-09 21:01:10 +0200

    Fix sample code

--- src/main/java/lt/akademija/itacademymanager/model/Student.java
+++ src/main/java/lt/akademija/itacademymanager/model/Student.java
@@ -4,9 +4,11 @@ import lombok.Data;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Data
 @Entity
+@Table("student")
 public class Student {
     @Id
     private Integer id;
