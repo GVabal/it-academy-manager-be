88cb6f732388f9e7ee57133e4a2f4ebe19df478b
GVabal <gvabal@gmail.com>
2021-03-09 20:40:31 +0200

    Initial commit

---
+++ src/main/java/lt/akademija/itacademymanager/ItAcademyManagerApplication.java
@@ -0,0 +1,13 @@
+package lt.akademija.itacademymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItAcademyManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItAcademyManagerApplication.class, args);
+    }
+
+}
