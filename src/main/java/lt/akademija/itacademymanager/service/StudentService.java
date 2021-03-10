1b543fe2b8599d86e59d07b0c9495df9cfbf46a7
Dziugas Untulis <un.dziugas@gmail.com>
2021-03-10 22:39:31 +0200

    Add edit Student

--- src/main/java/lt/akademija/itacademymanager/service/StudentService.java
+++ src/main/java/lt/akademija/itacademymanager/service/StudentService.java
@@ -1,6 +1,7 @@
 package lt.akademija.itacademymanager.service;
 
 import lombok.AllArgsConstructor;
+import lt.akademija.itacademymanager.exception.StudentNotFoundException;
 import lt.akademija.itacademymanager.model.Student;
 import lt.akademija.itacademymanager.repository.StudentRepository;
 import org.springframework.stereotype.Service;
@@ -13,5 +14,13 @@ public class StudentService {
 
     public Student addStudent(Student student) {
         return studentRepository.save(student);
+    }
+
+    public Student updateStudent(Student newStudent, Integer id){
+        if(studentRepository.existsById(id)){
+            newStudent.setId(id);
+            return studentRepository.save(newStudent);
+        }
+        throw new StudentNotFoundException("Student does not exist");
     }
 }
