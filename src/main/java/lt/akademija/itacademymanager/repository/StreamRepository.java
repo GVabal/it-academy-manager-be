95b89164c934bbc83d7a686e8199e5fa881a37b7
Andrius Drachneris <andrius.drachneris@gmail.com>
2021-03-13 10:43:09 +0200

    Refactor: Removed unused code part in repository

--- src/main/java/lt/akademija/itacademymanager/repository/StreamRepository.java
+++ src/main/java/lt/akademija/itacademymanager/repository/StreamRepository.java
@@ -2,10 +2,8 @@ package lt.akademija.itacademymanager.repository;
 
 import lt.akademija.itacademymanager.model.Stream;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.transaction.annotation.Transactional;
 
 
 public interface StreamRepository extends JpaRepository<Stream, Integer> {
@@ -13,8 +11,4 @@ public interface StreamRepository extends JpaRepository<Stream, Integer> {
     @Query(value = "SELECT * FROM public.stream WHERE name= :name", nativeQuery = true)
     Stream getStreamByName(@Param("name") String name);
 
-    @Modifying
-    @Transactional
-    @Query(value = "DELETE FROM public.stream WHERE name= :name", nativeQuery = true)
-    void deleteByName(@Param("name") String name);
 }
