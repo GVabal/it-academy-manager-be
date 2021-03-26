1c3cce90ecd00b978bbe1fefd4618f2ff6686f44
GabrieleB <buivydaite.gabriele@gmail.com>
2021-03-26 17:11:16 +0200

    removed authorizeRequests()

--- src/main/java/lt/akademija/itacademymanager/security/SecurityConfiguration.java
+++ src/main/java/lt/akademija/itacademymanager/security/SecurityConfiguration.java
@@ -22,9 +22,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().
-                cors().and().authorizeRequests()
-                .and().httpBasic();
+        http.csrf().disable().cors().and().httpBasic();
     }
 
     @Override
