7bd2c53687273bf50a3330b07b651e6d90426260
Andrius Drachneris <andrius.drachneris@gmail.com>
2021-03-27 09:40:56 +0200

    token works

--- src/main/java/lt/akademija/itacademymanager/config/JwtTokenUtil.java
+++ src/main/java/lt/akademija/itacademymanager/config/JwtTokenUtil.java
@@ -2,19 +2,14 @@ package lt.akademija.itacademymanager.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.gson.io.GsonDeserializer;
 import io.jsonwebtoken.gson.io.GsonSerializer;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
-import javax.crypto.spec.SecretKeySpec;
 import java.io.Serializable;
 import java.security.Key;
-import java.util.Base64;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -24,11 +19,9 @@ import java.util.function.Function;
 @Component
 public class JwtTokenUtil implements Serializable {
 
-    private static final long serialVersionUID = -2550185165626007488L;
-
     public static final long JWT_TOKEN_VALIDITY = 15 * 60;
-
-    private String secret = "2qUnPcOgX1b6bACWnk49tlfdWbI0f93F9igizvA2kb6oBrIavkl73520ut2qA2J";
+    private static final long serialVersionUID = -2550185165626007488L;
+    private final String secret = "2qUnPcOgX1b6bACWnk49tlfdWbI0f93F9igizvA2kb6oBrIavkl73520ut2qA2J";
 
     Key key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret));
 
@@ -46,6 +39,7 @@ public class JwtTokenUtil implements Serializable {
         final Claims claims = getAllClaimsFromToken(token);
         return claimsResolver.apply(claims);
     }
+
     //for retrieveing any information from token we will need the secret key
     private Claims getAllClaimsFromToken(String token) {
         return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody();
